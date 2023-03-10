527b79587266f1b06d8373907e14acb28d8bea3f
IvanAstr <IvanAst381@gmail.com>
2023-03-10 19:33:30 +0700

    v2.2

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,15 +5,23 @@ import { Home }from './pages/Home';
 import { About } from './pages/About';
 import { Catalogs } from './pages/Catalogs';
 import { Product } from './pages/Product';
+import { NotFound } from './pages/NotFound';
+import { Contacts } from './pages/Contacts';
+
+
 
 export const UseRouter = () => {
     return (
         <Routes>
-            <Route path="/main" element={<Home/>}/>
+            
+            <Route index path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
             <Route path="/catalogs/:id" element={<Catalogs/>}/>
             <Route path="/product/:id" element={<Product/>}/>
+            <Route path="/contacts" element={<Contacts/>}/>
+            <Route path="*" element={<NotFound/>}/>
 
         </Routes>
+        
     );
 }
